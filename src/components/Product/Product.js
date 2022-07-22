ea4e735ca976c25d0309b642f0ce65e8daaec706
hossein Rahmati <rmthossein@gmail.com>
2022-07-22 12:23:13 +0430

    Add delete, increment, decrement, change and render handlers

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,30 @@
-// import "./product.css";
 import styles from "./product.module.css";
+import { BiTrash } from "react-icons/bi";
+import { BiPlus } from "react-icons/bi";
+import { BiMinus } from "react-icons/bi";
 
 const Product = (props) => {
   return (
-    <div className={styles.product} onClick={props.click}>
-      <p>product name: {props.title}</p>
-      <p>product price: {props.price}</p>
+    <div className={styles.product}>
+      <p>{props.title} course</p>
+      <p>{props.price}</p>
+      <input type="text" value={props.title} onChange={props.onChange} />
+
+      {props.quantity === 1 ? (
+        <button className={styles.delete} onClick={props.onDelete}>
+          <BiTrash />
+        </button>
+      ) : (
+        <button className={styles.decrement} onClick={props.onDecrement}>
+          <BiMinus />
+        </button>
+      )}
+
+      <span className={styles.quantity}>{props.quantity}</span>
+
+      <button className={styles.increment} onClick={props.onIncrement}>
+        <BiPlus />
+      </button>
     </div>
   );
 };
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -5,25 +5,67 @@ import styles from "./ProductList.module.css";
 class ProductList extends Component {
   state = {
     products: [
-      { title: "react.js", price: "89 $", id: 1 },
-      { title: "react.js", price: "89 $", id: 2 },
-      { title: "react.js", price: "89 $", id: 3 },
+      { title: "react.js", quantity: 1, price: "59 $", id: 1 },
+      { title: "Vue.js", quantity: 1, price: "89 $", id: 2 },
+      { title: "Node.js", quantity: 1, price: "29 $", id: 3 },
     ],
   };
 
-  render() {
+  deleteHandler = (id) => {
+    const filteredProducts = this.state.products.filter((p) => p.id !== id);
+    this.setState({ products: filteredProducts });
+  };
+
+  incrementHandler = (id) => {
+    const products = [...this.state.products];
+    const incrementedProduct = products.find((p) => p.id === id);
+    incrementedProduct.quantity++;
+    this.setState({ products });
+  };
+
+  decrementHandler = (id) => {
+    const products = [...this.state.products];
+    const incrementedProduct = products.find((p) => p.id === id);
+    incrementedProduct.quantity--;
+    this.setState({ products });
+  };
+
+  changeHandler = (event, id) => {
+    const products = [...this.state.products];
+    const changeProduct = products.find((p) => p.id === id);
+    changeProduct.title = event.target.value;
+    this.setState({ products });
+  };
+
+  renderProduct = () => {
+    if (this.state.products.length === 0)
+      return <div>there is no product in cart</div>;
+
     return (
       <div className={styles.productList}>
         {this.state.products.map((product) => {
           return (
             <Product
+              key={product.id}
               title={product.title}
               price={product.price}
-              key={product.id}
-              click={this.clickHandler}
+              onChange={(e) => this.changeHandler(e, product.id)}
+              onIncrement={() => this.incrementHandler(product.id)}
+              quantity={product.quantity}
+              onDecrement={() => this.decrementHandler(product.id)}
+              onDelete={() => this.deleteHandler(product.id)}
             />
           );
         })}
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        {!this.state.products.length && <div>go to shopping!</div>}
+        {this.renderProduct()}
       </div>
     );
   }
